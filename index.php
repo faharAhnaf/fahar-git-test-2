f6014f7205c46319a8f30e0407c894fcdae2d1d5
FaharAhnafAzis <faharahnaf66@gmail.com>
2023-11-15 19:14:57 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
   <h1>Hello World!</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus ipsam odio, ea natus, officia, officiis nobis adipisci sequi cumque voluptas facilis. Quaerat culpa sapiente iste error velit pariatur voluptas accusamus.</p>
+  <ul>
+    <li>satu</li>
+  </ul>
 </body>
 </html>
